974a75f13b547bdb119a8642d39354f969cdb171
Anton Maurer <developer@maureranton.com>
2026-05-17 14:38:30 +0100

    Fix: remove RoomLastEvent parameter from RoomSummaryDataSource (missing Dagger binding)

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/room/summary/RoomSummaryDataSource.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/session/room/summary/RoomSummaryDataSource.kt
@@ -60,7 +60,6 @@ import javax.inject.Inject
 
 internal class RoomSummaryDataSource @Inject constructor(
         @SessionDatabase private val monarchy: Monarchy,
-        private val roomLastEvent: RoomLastEvent,
         private val clock: Clock,
         private val buildTaskValue: BuildRoomSummariesTaskValue,
 ) {
